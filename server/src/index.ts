3d2bc930e60c8c7b99becb4adad5a30de8a78311
vietduc-dev <levietduc.dev@gmail.com>
2025-07-17 06:55:08 +0700

    error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,9 @@ import { config } from "./config/app.config";
 import { asyncHandler } from "./middlewares/asyncHandler";
 import { HTTPSTATUS } from "./config/http.config";
 import connectDatabase from "./database/db";
+import { errorHandler } from "./middlewares/errorHandler";
+import { BadRequestException } from "./common/utils/catch-errors";
+import { ErrorCode } from "./common/enums/error-code.enum";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -24,11 +27,17 @@ app.use(cookieParser());
 app.get(
   "/",
   asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException(
+      "Bad request",
+      ErrorCode.AUTH_EMAIL_ALREADY_EXISTS
+    );
     res.status(HTTPSTATUS.OK).json({
       message: "Online Invitation!!!",
     });
   })
 );
+
+app.use(errorHandler);
 
 app.listen(config.PORT, async () => {
   console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
