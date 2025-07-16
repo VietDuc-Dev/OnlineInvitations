ee646e06ac89fb67c5188b08e4af27aace9ad62d
vietduc-dev <levietduc.dev@gmail.com>
2025-07-16 23:31:00 +0700

    server setup

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import { config } from "./config/app.config";
+import { asyncHandler } from "./middlewares/asyncHandler";
+import { HTTPSTATUS } from "./config/http.config";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: config.APP_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Online Invitation!!!",
+    });
+  })
+);
+
+app.use(cookieParser());
+
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+});
